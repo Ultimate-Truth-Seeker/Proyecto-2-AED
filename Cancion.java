b92d4031b826360cdce3b9fadf0aa42ce9a4130a
Luis Palacios <luisferpalacioslopez@gmail.com>
2024-04-28 17:38:53 -0600

    Create Cancion.java

---
+++ Cancion.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Cancion {
+    private String nombre;
+    private double[] caracteristicas;
+
+    // Constructor
+    public Cancion(String nombre, double[] caracteristicas) {
+        this.nombre = nombre;
+        this.caracteristicas = caracteristicas;
+    }
+
+}
