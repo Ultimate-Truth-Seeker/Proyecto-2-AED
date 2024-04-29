c9875cd0f5da9d420c86ca22fd7105df8660d7fb
Estuardo Castro <cas23890@uvg.edu.gt>
2024-04-28 18:17:35 -0600

    Crear método conexión

--- Main.java
+++ Main.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String args[]) {
         // crea el grafo dado en la figura anterior
-        Grafo<String> graph = new Grafo<String>(new ArrayList<>());
+        Grafo<String> graph = new Grafo<String>(List.of());
         graph.addEdge("Usuario1", "Canción1");
         graph.addEdge("Usuario1", "Canción2");
         graph.addEdge("Usuario2", "Canción1");
@@ -15,5 +16,13 @@ public class Main {
 
         // imprime la representación de la lista de adyacencia del grafo anterior
         graph.printGraph();
+
+    }
+
+    private static void conexion(Grafo<String> grafo, double umbral) {
+        
+        List<Cancion> canciones = new ArrayList<>();
+
+
     }
 }
