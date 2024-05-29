2bed853b7f67c2abe05339159688acdcda932c14
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-05-28 20:39:12 -0600

    conectar api de node4j

--- script.py
+++ script.py
@@ -19,3 +19,43 @@ total = 150
 
 tracks = obtener_datos_global(api_key, "guatemala", total)["tracks"]["track"]
 
+
+
+import dotenv
+import os
+from neo4j import GraphDatabase
+
+load_status = dotenv.load_dotenv("Neo4j-07e5e662-Created-2024-05-20.txt")
+if load_status is False:
+    raise RuntimeError('Environment variables not loaded.')
+
+URI = "neo4j+ssc://07e5e662.databases.neo4j.io"
+AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
+
+with GraphDatabase.driver(URI, auth=AUTH) as driver:
+    driver.verify_connectivity()
+    
+
+    with driver.session(database="neo4j") as session:
+            count = 0
+            for song in tracks:
+                count +=1;
+                name = song["name"]
+                duration = song["duration"]
+                listeners = song["listeners"]
+                artist = song["artist"]["name"]
+                datos = obtener_datos_cancion(api_key, artist, name)["track"]
+                album = ""; albumartist = "";
+                try:
+                    album = datos["album"]["title"]
+                    albumartist = datos["album"]["artist"]
+                except:
+                    "no album"
+                playcount = datos["playcount"]
+                tagsraw = datos["toptags"]["tag"]
+                tags = []
+                for tag in tagsraw:
+                    tags.append(tag["name"])
+                
+            print("done")
+                
