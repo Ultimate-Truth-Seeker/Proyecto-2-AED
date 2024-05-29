f7e247e23ce920f37cb87d401b68692471da2d2d
Luis Palacios <luisferpalacioslopez@gmail.com>
2024-05-28 22:04:26 -0600

    buscar y obtener cancion
    buscar opción por su nombre y función para obtener la canción que se busca

--- app.py
+++ app.py
@@ -13,3 +13,18 @@ RETURN n AS tags
 ORDER BY COUNT{(s:song)-[r:is]-(n)} DESC
 LIMIT 5;
 """
+get_songs_bytag = """
+MATCH (n:tag {name: $tag})-[r:is]-(s:song)
+MATCH (s)-[u:from]-(a:artist)
+MATCH (s)-[v:from]-(b:album)
+RETURN s AS song, a AS artist, b AS album
+ORDER BY s.playcount DESC
+LIMIT 5
+"""
+
+get_song = """
+MATCH (s:song {name: $song})
+MATCH (s)-[r:from]-(a:artist)
+MATCH (s)-[t:from]-(b:album)
+RETURN s AS song, a AS artist, b AS album
+"""
